af7291a5361c5b2d2afc6823b03f7f2099be82aa
Niklas Bieck <niklas.bieck@digipen.edu>
2015-09-02 11:07:46 -0700

    Add testing code

--- ObjLoad/main.cpp
+++ ObjLoad/main.cpp
@@ -1,7 +1,31 @@
 //author: Niklas Bieck
 //just a basic testing main for the .obj loading
 
+#include <vector>
+#include <iostream>
+
+#include "ObjLoader.h"
+
 int main(void)
 {
+	ObjLoader load;
 
+	std::vector<ObjLoader::Vertex> vertices;
+	std::vector<ObjLoader::Index> indices;
+
+	load.Load("box.obj", vertices, indices);
+
+	std::cout << "Vertices:" << std::endl;
+	for (auto v : vertices)
+	{
+		std::cout << "Pos:\t" << v.position << "\tUV:\t" << v.tex_coord << "\tNorm:\t" << v.normal << std::endl;
+	}
+
+	std::cout << "Indices:" << std::endl;
+	for (auto i : indices)
+	{
+		std::cout << i << std::endl;
+	}
+
+	system("PAUSE");
 }
